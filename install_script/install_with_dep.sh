adf59d7a8a4acc0a0593eafce3f351a3d2e8590e
Yossi Jana <yossijana@gmail.com>
2020-05-12 19:05:23 +0300

    call install from dep script

--- install_script/install_with_dep.sh
+++ install_script/install_with_dep.sh
@@ -28,25 +28,5 @@ else
     exit 1
 fi
 
-# get vim-plug file
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-      https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -sL https://github.com/yossij/vimrc/raw/master/install_script/install.sh | bash -
 
-# get vim configuration files from repo
-git clone https://github.com/yossij/vimrc.git
-
-# save the vim files in ~/.vim/ dir
-shopt -s dotglob
-mv -f vimrc/* ~/.vim/
-rmdir vimrc
-
-# source the vim files in .vimrc
-cat <<EOT >> ~/.vimrc
-source ~/.vim/sets.vim
-source ~/.vim/vim-plug.vim
-source ~/.vim/colorscheme.vim
-source ~/.vim/coc.vim
-EOT
-
-# install plugins from vim and quit
-vim +PlugInstall +qall
