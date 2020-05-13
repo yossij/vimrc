d08466b46018c1d82b50cbacd0f6f1cff6f72c00
Yossi Jana <yossijana@gmail.com>
2020-05-13 13:05:06 +0300

    cleanup

--- install_script/install_with_dep.sh
+++ install_script/install_with_dep.sh
@@ -4,34 +4,34 @@
 
 # yum
 if [[ `command -v yum | grep -o yum` == 'yum' ]]; then
-	# install git
-	sudo yum install git -y
+  # install git
+  sudo yum install git -y
 
-	# install nodsjs for coc
-	curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
-	sudo yum install -y nodejs
+  # install nodsjs for coc
+  curl -sL https://rpm.nodesource.com/setup_12.x | sudo bash -
+  sudo yum install -y nodejs
 
 # mac
 elif [[ `command -v brew | grep -o brew` == 'brew' ]]; then
   # install git
   # and nodsjs (for coc)
-	brew install -y git node
+  brew install -y git node
 
 # apt
 elif [[ `command -v apt | grep -o apt` == 'apt' ]]; then
-	# update apt
-	sudo apt update
+  # update apt
+  sudo apt update
 
-	# install git
-	sudo apt install git -y
+  # install git
+  sudo apt install git -y
 
-	# install git 
+  # install git 
   # and nodsjs, npm (for coc)
-	sudo apt install git nodejs npm -y
+  sudo apt install git nodejs npm -y
 
 else
-    echo "Unsupported OS"
-    exit 1
+  echo "Unsupported OS"
+  exit 1
 fi
 
 curl -sL https://github.com/yossij/vimrc/raw/master/install_script/install.sh | bash -
